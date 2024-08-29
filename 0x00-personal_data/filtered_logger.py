3ab0d1805f45904a5e1f4aabba504272a0e46ae3
mainbosskay <dadakehinde80@yahoo.com>
2024-08-29 12:42:45 +0000

    Python program that implements format method to filter values in incoming log records using filter_datum. Values for fields in fields should be filtered.

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,6 +2,7 @@
 """Module that handles filtering of log"""
 from typing import List
 import re
+import logging
 
 
 regexPattrn = {
@@ -16,3 +17,23 @@ def filter_datum(fields: List[str], redaction: str,
     """Getting and return log message obfuscated"""
     extract, replace = (regexPattrn["extract"], regexPattrn["replace"])
     return re.sub(extract(fields, separator), replace(redaction), message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """Redacting Formatter class"""
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """Initializing class for RedactingFormatter"""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Formats a LogRecord and conceals designated fields"""
+        mssgeFmt = super(RedactingFormatter, self).format(record)
+        mssgeRedact = filter_datum(self.fields,  self.REDACTION,
+                                   mssgeFmt, self.SEPARATOR)
+        return mssgeRedact
