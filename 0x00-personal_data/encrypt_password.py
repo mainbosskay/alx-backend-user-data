2f7de73b7efb08d92c741db3dce8ce41a2d631a0
mainbosskay <dadakehinde80@yahoo.com>
2024-08-29 14:16:19 +0000

    Python program that implements is_valid function that expects 2 arguments and returns a boolean

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -6,3 +6,8 @@ import bcrypt
 def hash_password(password: str) -> bytes:
     """Getting and return salted, hashed password with bcrypt"""
     return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Validating plain password using hashed password"""
+    return bcrypt.checkpw(password.encode("utf-8"), hashed_password)
