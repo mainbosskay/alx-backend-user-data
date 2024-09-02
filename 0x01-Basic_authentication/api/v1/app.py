56d533826689b87881a7fa40500d2ff95fb2b9da
mainbosskay <dadakehinde80@yahoo.com>
2024-09-02 23:06:46 +0000

    Updating method in api/v1/app.py to handler before_request

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -7,11 +7,16 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
+from api.v1.auth.auth import Auth
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+authType = getenv("AUTH_TYPE", "auth")
+if authType == "auth":
+    auth = Auth()
 
 
 @app.errorhandler(404)
@@ -31,6 +36,22 @@ def unauthorized(error) -> str:
 def forbidden(error) -> str:
     """Forbidden handler"""
     return jsonify({"error": "Forbidden"}), 403
+
+
+@app.before_request
+def check_auth() -> str:
+    """Setting up Flask before_request to check user auth & authorization"""
+    if auth:
+        excluded_paths = ['/api/v1/status/',
+                          '/api/v1/unauthorized/',
+                          '/api/v1/forbidden/']
+        if auth.require_auth(request.path, excluded_paths):
+            authHeader = auth.authorization_header(request)
+            userNow = auth.current_user(request)
+            if authHeader is None:
+                abort(401)
+            if userNow is None:
+                abort(403)
 
 
 if __name__ == "__main__":
