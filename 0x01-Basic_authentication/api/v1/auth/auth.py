c1a5903c6228323f4ca990861249f86656833646
mainbosskay <dadakehinde80@yahoo.com>
2024-09-02 22:35:31 +0000

    Creating new folder auth and create class Auth with basic method

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""Module for API authentication operations management"""
+from flask import request
+import re
+from typing import List, TypeVar
+
+
+class Auth:
+    """Class for managing authentication operations"""
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Determines if authentication is requred for a given path"""
+        if path is not None and excluded_paths is not None:
+            for excldPath in map(lambda k: k.strip(), excluded_paths):
+                matchPattrn = ""
+                if excldPath[-1] == "*":
+                    matchPattrn = f"{excldPath[0:-1]}.*"
+                elif excldPath[-1] == "/":
+                    matchPattrn = f"{excldPath[0:-1]}/*"
+                else:
+                    matchPattrn = f"{excldPath}/*"
+                if re.match(matchPattrn, path):
+                    return False
+
+
+    def authorization_header(self, request=None) -> str:
+        """Getting and return auth header from incoming request"""
+        return None
+
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Identifying present user based on the request"""
+        return None
