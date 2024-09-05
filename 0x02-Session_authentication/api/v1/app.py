009f3933c1a028d7a8860082729d1ebb43319668
mainbosskay <dadakehinde80@yahoo.com>
2024-09-05 02:29:30 +0000

    Implementing Basic authentication for giving you access to all User endpoints

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -50,12 +50,11 @@ def check_auth() -> str:
     if auth:
         excluded_paths = ['/api/v1/status/',
                           '/api/v1/unauthorized/',
-                          '/api/v1/forbidden/',
-                          '/api/v1/auth_session/login/']
+                          '/api/v1/forbidden/',]
         if auth.require_auth(request.path, excluded_paths):
+            authHeader = auth.authorization_header(request)
             userNow = auth.current_user(request)
-            if auth.authorization_header(request) is None and \
-                    auth.session_cookie(request) is None:
+            if authHeader is None:
                 abort(401)
             if userNow is None:
                 abort(403)
