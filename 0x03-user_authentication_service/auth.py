1ed219dafd6e54f7dfa1d5eb6b032264937540ba
mainbosskay <dadakehinde80@yahoo.com>
2024-09-11 16:30:20 +0000

    Implementing _generate_uuid function in the auth module. The function should return a string representation of a new UUID

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -4,11 +4,17 @@ import bcrypt
 from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
+from uuid import uuid4
 
 
 def _hash_password(password: str) -> bytes:
     """Encrypting and return password salted hash"""
     return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+
+
+def _generate_uuid() -> str:
+    """Getting and return unique uuid"""
+    return str(uuid4())
 
 
 class Auth:
