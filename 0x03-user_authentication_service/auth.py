db364d815bd642b029c3bed12bcd293e26e349cd
mainbosskay <dadakehinde80@yahoo.com>
2024-09-11 16:54:53 +0000

    Implementing update_password function in the app module to respond to the PUT /reset_password route

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -5,6 +5,7 @@ from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
 from uuid import uuid4
+from typing import Union
 
 
 def _hash_password(password: str) -> bytes:
@@ -57,3 +58,46 @@ class Auth:
         session_id = _generate_uuid()
         self._db.update_user(userRecord.id, session_id=session_id)
         return session_id
+
+    def get_user_from_session_id(self, session_id: str) -> Union[User, None]:
+        """Getting and return user using session ID"""
+        userRecord = None
+        if session_id is None:
+            return None
+        try:
+            userRecord = self._db.find_user_by(session_id=session_id)
+        except NoResultFound:
+            return None
+        return userRecord
+
+    def destroy_session(self, user_id: int) -> None:
+        """Terminating specific user linked session"""
+        if user_id is None:
+            return None
+        self._db.update_user(user_id, session_id=None)
+
+    def get_reset_password_token(self, email: str) -> str:
+        """Creating reset token password for specified email"""
+        userRecord = None
+        try:
+            userRecord = self._db.find_user_by(email=email)
+        except NoResultFound:
+            userRecord = None
+        if userRecord is None:
+            raise ValueError()
+        resetTokPwd = _generate_uuid()
+        self._db.update_user(userRecord.id, reset_token=resetTokPwd)
+        return resetTokPwd
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """Updating user's password with provided reset token"""
+        instUser = None
+        try:
+            instUser = self._db.find_user_by(reset_token=reset_token)
+        except NoResultFound:
+            instUser = None
+        if instUser is None:
+            raise ValueError()
+        newPWDhash = _hash_password(password)
+        self._db.update_user(instUser.id, hashed_password=newPWDhash,
+                             reset_token=None)
